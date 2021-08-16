ed43880530c1873c9e75ee093625b2f1a247aabf
yunakash <nakashi1000@gmail.com>
2021-08-16 17:16:07 +0900

    cp00

---
+++ CPC02/ft_strcpy.c
@@ -0,0 +1,17 @@
+char *ft_strcpy(char *dest, char *src);
+int main(){
+	char dest[5] = 'aiueo';
+	char src[5];
+
+	char *dest, *src;
+	dest
+
+	ft_strcpy(dest, src);
+	printf("%c\n%c",dest,src);
+	return 0;
+}
+
+void	*ft_strcpy(char *dest, char *src)
+{
+	
+}
