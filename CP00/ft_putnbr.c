c1ade483048777e053f013af8b81bb444c197df4
yunakash <nakashi1000@gmail.com>
2021-08-20 16:34:06 +0900

    a

--- CP00/ft_putnbr.c
+++ CP00/ft_putnbr.c
@@ -1,23 +1,16 @@
 #include <unistd.h>
-void	put_char(int g_n);
-int		put_n(int g_n);
-int		ft_pow(int g_dn);
+void	put_char(int n);
+int		put_n(int n);
+int		ft_pow(int dn);
 void	cal_num(int nb);
-int		g_nb = 0;
-int		g_n = 0;
-int		g_dn = 0;
-int		g_dtotal = 0;
-int		g_ex = 0;
 
 void	ft_putnbr(int nb)
 {
-	g_nb = 0;
-	g_n = 0;
-	g_dn = 0;
-	g_dtotal = 0;
-	g_ex = 0;
-	if (nb > 2147483647 || nb < -2147483648)
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 12);
 		return ;
+	}
 	if (nb == 0)
 		put_char(0);
 	if (nb < 0)
@@ -27,52 +20,56 @@ void	ft_putnbr(int nb)
 
 void	cal_num(int nb)
 {
-	g_nb = nb;
+	int	dn;
+	int	dtotal;
+	int	n;
+
 	while (nb > 0)
 	{
 		nb = nb / 10;
-		g_dn++;
+		dn++;
 	}
-	g_dtotal = ft_pow(g_dn);
-	while (g_dn > 0)
+	dtotal = ft_pow(dn);
+	while (dn > 0)
 	{
-		g_n = g_nb / g_dtotal;
-		put_char(g_n);
-		g_nb = g_nb % g_dtotal;
-		g_dtotal = g_dtotal / 10;
-		g_dn--;
+		n = nb / dtotal;
+		put_char(n);
+		nb = nb % dtotal;
+		dtotal = dtotal / 10;
+		dn--;
 	}
 }
 
-void	put_char(int g_n)
+void	put_char(int n)
 {
 	char	c;
 
-	c = 48 + g_n;
+	c = 48 + n;
 	write(1, &c, 1);
 }
 
-int	ft_pow(int g_dn)
+int	ft_pow(int dn)
 {
 	int	pow;
 
 	pow = 1;
-	while (g_dn > 1)
+	while (dn > 1)
 	{
 		pow = 10 * pow;
-		g_dn--;
+		dn--;
 	}
 	return (pow);
 }
 
-int	put_n(int g_n)
+int	put_n(int n)
 {
-	g_n = g_n * -1;
+	n = n * -1;
 	write(1, "-", 1);
-	return (g_n);
+	return (n);
 }
 
-int main()
+/*int main()
 {
-	ft_putnbr(43242);
+	ft_putnbr(-2147483648);
 }
+*/
